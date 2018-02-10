c46779935d273c0e9494734d4fc479e26c16fd6f
David <zzhangddavid@gmail.com>
2018-02-10 14:09:19 -0800

    add name paramenter to login

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 var users = {
+	"vivan" : {
+		"name": "Vivian Chung"
+		"username": "bibz",
+		"password": "asdf",
+		"email" : "zzhangddavid@gmail.com"
+	},
 	"johnson" : {
-		"username": "johnson",
+		"name": "Johnsun Sun",
+		"username": "flyinpiggy",
 		"password": "asdf",
 		"email" : "zzhangddavid@gmail.com"
 	},
 	"david" : {
-		"username": "david",
+		"name": "David Zhang"
+		"username": "skyiky",
+		"password": "asdf",
+		"email" : "zzhangddavid@gmail.com"
+	},
+	"blaine" : {
+		"name": "Blaine Huynh"
+		"username": "spiffykid",
+		"password": "asdf",
+		"email" : "zzhangddavid@gmail.com"
+	},
+	"nessa": {
+		"name": "Vanessa Harrison"
+		"username": "Lunessa",
 		"password": "asdf",
 		"email" : "zzhangddavid@gmail.com"
 	}
